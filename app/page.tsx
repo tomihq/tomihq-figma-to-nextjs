7a8c00e59fe6ecb419ad390c0b6c50cacbcbaa53
= <hernandeztomas584@gmail.com>
2022-12-01 01:19:58 -0300

    add: nextjs12 and tailwind

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-    return (
-   
-        <h1 className="text-red-500">Hello World</h1>
- 
-    )
-  }
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <h1 className='text-red-300'> Hola Mundo</h1>
+    </>
+  )
+}
+
+export default HomePage
